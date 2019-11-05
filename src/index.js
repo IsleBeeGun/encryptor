1c7cf38f6d3ab3f2ed02e6431406bb92ee40571a
IsleBeeGun <islebeegun@gmail.com>
2019-11-05 18:03:42 +0300

    Fix name and description in manifest.json and index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import './reset.css';
-import './index.css';
+import './reset.scss';
+import './index.scss';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
