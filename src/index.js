a92b733d53f57507f79739a0b8d95052559acb56
IsleBeeGun <islebeegun@gmail.com>
2019-11-05 14:10:24 +0300

    Add reset.css (no npm pack)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import './reset.css';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
