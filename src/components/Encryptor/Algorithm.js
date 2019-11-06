ff3eac465b4d12dc2be48627dfa67207e3d48d9f
IsleBeeGun <islebeegun@gmail.com>
2019-11-06 20:39:31 +0300

    Messing around

---
+++ src/components/Encryptor/Algorithm.js
@@ -0,0 +1,10 @@
+const Algorithm = {
+    encrypt(key, text) {
+
+    },
+    decrypt(key, text) {
+
+    }
+}
+
+export default Algorithm
--- src/components/Encryptor/Key/Key.jsx
+++ src/components/Encryptor/Key/Key.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import "./Key.scss";
 
 class Key extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: ''
+    };
+    this.handleEncryption = this.handleEncryption.bind(this);
+    this.handleKeyChange = this.handleKeyChange.bind(this);
+  }
+  handleEncryption() {
+    // this.props.searchGoogle(this.state.keyword, this.state.type, this.state.sortBy);
+  }
+  async handleKeyChange(event) {
+    await this.setState({ key: event.target.value });
+    this.handleEncryption();
+  }
   render() {
     return (
       <div className="Key row">
         <div className="col bg-warning">
-          <p>Key</p>
+          <input placeholder="Type your password here.." onChange={this.handleKeyChange} />
         </div>
       </div>
     );
