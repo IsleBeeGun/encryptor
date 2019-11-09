eb24b893120b15d24bc9845c3d58b51db93bd10c
IsleBeeGun <IsleBeeGun@gmail.com>
2019-11-09 20:36:17 +0300

    Messing

--- src/components/Encryptor/Algorithm.js
+++ src/components/Encryptor/Algorithm.js
@@ -14,7 +14,11 @@ const Algorithm = {
     console.log(`%c${state.rawText[state.rawText.length - 1].rawText}`,'color: white; background-color: green');
     console.log(`%c${state.encryptedText[state.encryptedText.length - 1].encryptedText}`,'color: white; background-color: red');
 
-    
+    let test = '';
+    for (let i=0; i < state.key[state.key.length - 1].key.length; i++ ) {
+        test += state.key[state.key.length - 1].key.charCodeAt(i) + ',';
+    }
+    console.log(`%c${test}`,'color: white; background-color: black');
   }
 };
 
