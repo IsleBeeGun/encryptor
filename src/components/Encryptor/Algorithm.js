6c5ce96bd7a68743d7b5896a84228799d0a9de89
IsleBeeGun <IsleBeeGun@gmail.com>
2019-11-09 13:35:17 +0300

    Algorithm.js: Change console.log msgs for tracking active

--- src/components/Encryptor/Algorithm.js
+++ src/components/Encryptor/Algorithm.js
@@ -2,11 +2,11 @@ import { store } from "../../store/store";
 const Algorithm = {
   perform() {
     console.clear();
-    console.log(store.getState());
+    // console.log(store.getState());
     let state = store.getState();
-    console.log(state.key[state.key.length - 1].isActive);
-    console.log(state.rawText[state.rawText.length - 1].isActive);
-    console.log(state.encryptedText[state.encryptedText.length - 1].isActive);
+    console.log(`Key field: ${state.key[state.key.length - 1].isActive}`);
+    console.log(`Raw text field: ${state.rawText[state.rawText.length - 1].isActive}`);
+    console.log(`Encrypted field: ${state.encryptedText[state.encryptedText.length - 1].isActive}`);
   }
 };
 
