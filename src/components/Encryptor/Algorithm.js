fa6eb08beb5ec7c226268777a5579cae38c596ac
IsleBeeGun <islebeegun@gmail.com>
2019-11-11 15:25:26 +0300

    Algorithm.js: Fix remained task variable in console.log

--- src/components/Encryptor/Algorithm.js
+++ src/components/Encryptor/Algorithm.js
@@ -8,7 +8,6 @@ const Algorithm = {
     console.log(`Key field: ${state.key[state.key.length - 1].isActive}`);
     console.log(`Raw text field: ${state.rawText[state.rawText.length - 1].isActive}`);
     console.log(`Encrypted field: ${state.encryptedText[state.encryptedText.length - 1].isActive}`);
-    console.log(`Current task: ${state.task[state.task.length - 1].task}`);
     
     console.log(`%c${state.key[state.key.length - 1].key}`,'color: white; background-color: orange');
     console.log(`%c${state.rawText[state.rawText.length - 1].rawText}`,'color: white; background-color: green');
