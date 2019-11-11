e191e873720603b8b4baf01604040c21896f69f6
IsleBeeGun <islebeegun@gmail.com>
2019-11-11 17:27:14 +0300

    Apply same logic (export default connect..) to Key component

--- src/components/Encryptor/Key/Key.jsx
+++ src/components/Encryptor/Key/Key.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import "./Key.scss";
-import { store, provideKey } from '../../../store/store';
-import Algorithm from '../Algorithm';
+import { store, provideKey } from "../../../store/store";
+import Algorithm from "../Algorithm";
+import {connect} from "react-redux";
 
 class Key extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      key: ''
-    };
     this.handleKeyChange = this.handleKeyChange.bind(this);
   }
   async handleKeyChange(event) {
-    await this.setState({ key: event.target.value });
-    await store.dispatch(provideKey(this.state.key));
+    await store.dispatch(provideKey(event.target.value));
     Algorithm.perform();
   }
   render() {
     return (
       <div className="Key row">
         <div className="col bg-warning">
-          <input placeholder="Your password.." onChange={this.handleKeyChange} />
+          <input
+            placeholder="Your password.."
+            onChange={this.handleKeyChange}
+          />
         </div>
       </div>
-        );
-      }
-    }
-    
-    export default Key;
+    );
+  }
+}
+export default connect(
+  state => ({
+    storeState: state
+  }),
+  dispatch => ({})
+)(Key);
