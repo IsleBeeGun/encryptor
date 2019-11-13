9eda71ac81044f695d7ac6186dd46f9b3814cbb0
IsleBeeGun <islebeegun@gmail.com>
2019-11-13 16:17:31 +0300

    fix footer heading and path to icon

--- src/components/MainFooter/MainFooter.jsx
+++ src/components/MainFooter/MainFooter.jsx
@@ -6,7 +6,7 @@ class MainFooter extends React.Component {
     return (
       <footer className="MainFooter row">
         <div className="col bg-dark text-white-50">
-          <h1 className="text-right">..byIBG</h1>
+          <h1 className="text-right h1">..byIBG</h1>
         </div>  
       </footer>
     );
