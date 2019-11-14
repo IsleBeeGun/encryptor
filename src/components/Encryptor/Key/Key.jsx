2bcb5fdb90b126b4ef031f44bbb2dda1330ec54e
IsleBeeGun <IsleBeeGun@gmail.com>
2019-11-14 16:20:15 +0300

    Provide usage manual inside help modal

--- src/components/Encryptor/Key/Key.jsx
+++ src/components/Encryptor/Key/Key.jsx
@@ -66,7 +66,7 @@ class Key extends React.Component {
             <input
               type={this.state.showPassword}
               id="password"
-              placeholder="Provide your password.."
+              placeholder="Type here.."
               onChange={this.handleKeyChange}
               className="form-control"
             />
@@ -113,8 +113,9 @@ class Key extends React.Component {
           <div className="modal-dialog modal-dialog-centered" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="helpModalTitle">
-                  Modal title
+                <FontAwesomeIcon icon={faQuestionCircle} size="lg" />
+                <h5 className="modal-title ml-2" id="helpModalTitle">
+                  How does this work?
                 </h5>
                 <button
                   type="button"
@@ -125,7 +126,57 @@ class Key extends React.Component {
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div className="modal-body">...</div>
+              <div className="modal-body">
+                <p className="bg-warning pl-2 text-black">
+                  <strong>First of all</strong>, you need to input your secret{" "}
+                  <strong>key-word</strong> into the password field.
+                </p>
+                <p>
+                  <strong>Then</strong>, you can either encrypt your text, or
+                  decrypt previously encrypted text.
+                </p>
+                <p className="text-success">
+                  <strong>For encryption:</strong>
+                </p>
+                <p className="bg-success pl-2 text-white">
+                  Place your text into the green area under{" "}
+                  <strong>"Normal Text"</strong> label, receive encrypted text
+                  in the red area, under{" "}
+                  <strong className="bg-danger">
+                    &nbsp;"Encrypted&nbsp;Text"&nbsp;
+                  </strong>{" "}
+                  label.
+                </p>
+                <p className="text-danger">
+                  <strong>For decryption:</strong>
+                </p>
+                <p className="bg-danger pl-2 text-white">
+                  Place your previously encrypted text into the red area, under{" "}
+                  <strong>"Encrypted Text"</strong> label, receive decrypted
+                  text in the green area under{" "}
+                  <strong className="bg-success">
+                    &nbsp;"Normal&nbsp;Text"&nbsp;
+                  </strong>{" "}
+                  label.{" "}
+                </p>
+                <p className="text-info">
+                  <strong>Also notice:</strong>
+                </p>
+                <p>
+                  You can change your password/key-word anytime you like, but
+                  the changes won't appear immediately. In order to see changes,
+                  you should continue typing inside{" "}
+                  <strong className="bg-success text-white">
+                    &nbsp;"Normal&nbsp;Text"&nbsp;
+                  </strong>{" "}
+                  or{" "}
+                  <strong className="bg-danger text-white">
+                    &nbsp;"Encrypted&nbsp;Text"&nbsp;
+                  </strong>{" "}
+                  field, you'll see your text rearranged by new password shortly
+                  after.{" "}
+                </p>
+              </div>
               <div className="modal-footer">
                 <button
                   type="button"
@@ -133,9 +184,6 @@ class Key extends React.Component {
                   data-dismiss="modal"
                 >
                   Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Save changes
                 </button>
               </div>
             </div>
